e218a1480f0c8a759cd00abfc1f1c647d81f60e3
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-03-12 19:08:50 +0530

    host

--- services/translation.service.js
+++ services/translation.service.js
@@ -1,11 +1,15 @@
-const { translate } = require('free-translate');
+const { translate } = require('bing-translate-api');
 
-
-async function engToFr(reqData){
-  const translation = await translate(reqData, { from: 'en', to: 'fr' });
-  return translation ;
+async function engToFr(reqData) {
+  try {
+    const res = await translate(reqData, 'en', 'fr');
+    return res.translation;
+  } catch (err) {
+    console.error(err);
+    throw err; 
+  }
 }
 
-module.exports={
+module.exports = {
   engToFr,
-}
+};
